03e976673fce95e31bcdfb5d3736e16fe37e3aa1
Eugene Zavoloka <eugenezm@gmail.com>
2021-11-18 22:07:56 +0200

    added City Department

---
+++ app/Http/Controllers/Backend/DepartmentController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Department;
+use App\Models\City;
+use App\Http\Requests\DepartmentStoreRequest;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $departments = Department::all();
+        if ($request->has('search')) {
+            $departments = Department::where('name', 'like', "%{$request->search}%")->get();
+        }
+        return view('departments.index', compact('departments'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $cities = City::all();
+
+        return view('departments.create', compact('cities'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(DepartmentStoreRequest $request)
+    {
+        Department::create($request->validated());
+        return redirect()->route('departments.index')->with('message', 'Department Created Succesfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Department $department)
+    {
+        return view('departments.edit', compact('department'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(DepartmentStoreRequest $request, Department $department)
+    {
+        $department->update([
+            'name' => $request->name,
+        ]);
+        return redirect()->route('departments.index')->with('message', 'Department Updated Succesfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Department $department)
+    {
+        $department->delete();
+        return redirect()->route('departments.index')->with('message', 'Department Deleted Successfully');
+    }
+}
